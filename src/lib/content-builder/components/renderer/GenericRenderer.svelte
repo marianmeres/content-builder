fd77e98a626c4c4a5e396826fded5e8a4b5ca094
Marian Meres <marian@meres.sk>
2024-06-02 13:00:16 +0200

    generic renderer THC support

--- src/lib/content-builder/components/renderer/GenericRenderer.svelte
+++ src/lib/content-builder/components/renderer/GenericRenderer.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { Thc } from '@marianmeres/stuic';
+	import type { THC } from '@marianmeres/stuic';
 	import { twMerge } from 'tailwind-merge';
 
 	// toto je zoznam vsetkych defaultnych props, ktore vzdy chodia
@@ -6,13 +8,20 @@
 	export { _class as class };
 	export let key: string | undefined = '';
 	export let type: string | undefined = '';
-	export let html: string | undefined = '';
+	export let html: string | undefined | THC[] = '';
 	export let style: string | undefined = '';
 	export let hidden: boolean | undefined = false;
 	export let __typeIsValid: boolean = false;
 
 	// so we can have a "flexible spacer"
 	let _defaultClass = 'flex flex-col relative';
+
+	let _thcSegments: THC[] = [];
+	$: if (html) {
+		_thcSegments = Array.isArray(html) ? html : [html];
+	} else {
+		_thcSegments = [];
+	}
 </script>
 
 {#key key}
@@ -25,8 +34,13 @@
 		data-content-builder-node-type-invalid={__typeIsValid ? undefined : type}
 	>
 		<!-- important to wrap inside a div because of flexible spacers -->
-		{#if html}
+		<!-- {#if html}
 			<div>{@html html}</div>
+		{/if} -->
+		{#if _thcSegments.length}
+			{#each _thcSegments as thc}
+				<div><Thc {thc} forceAsHtml /></div>
+			{/each}
 		{/if}
 		<slot />
 	</div>
